5dda067d096786d59d7c1917101d1ee6683fbe3f
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2021-10-15 08:09:30 +0200

    a

--- js/search.js
+++ js/search.js
@@ -1,19 +1,111 @@
 import recipes from "./recipes.js";
-import displayRecipes from "./index.js";
+import displayRecipe from "./index.js";
 
-const search = () => {
+const principalSearch = () => {
   const searchUser = document.getElementById("searchInput");
   searchUser.addEventListener("input", (e) => {
     const element = e.target.value.toLowerCase();
     if (element.length >= 3) {
       console.log(element);
-      const newRecipe = recipes.filter((recipe) => {
-        if (recipe.name.toLowerCase().includes(element)) {
-          console.log(recipe.name);
+      document.getElementById("recipes").innerHTML = "";
+      recipes.filter((recipe) => {
+        if (
+          recipe.name.toLowerCase().match(element) ||
+          recipe.description.toLowerCase().match(element)
+        ) {
+          displayRecipe(recipe);
+          // searchIngredients();
+          console.log(recipe);
+
+          //second
+        } else if (
+          !recipe.name.toLowerCase().match(element) &&
+          !recipe.description.toLowerCase().match(element)
+        ) {
+          recipe.ingredients.forEach((el) => {
+            if (el.ingredient.toLowerCase().match(element)) {
+              console.log(el.ingredient);
+              displayRecipe(recipe);
+            }
+          });
+        }
+      });
+      if (document.getElementById("recipes").innerHTML === "") {
+        alert(
+          "« Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc."
+        );
+        // window.location.reload();
+        document.getElementById("recipes").innerHTML = "";
+        recipes.forEach((recipe) => {
+          displayRecipe(recipe);
+        });
+        console.log(element);
+      }
+    } else if (element.length === 0) {
+      document.getElementById("recipes").innerHTML = "";
+      recipes.forEach((recipe) => {
+        displayRecipe(recipe);
+      });
+    }
+  });
+};
+
+const searchAppareil = () => {
+  const searchUserAppareil = document.getElementById("searchAppareil");
+  searchUserAppareil.addEventListener("input", (e) => {
+    const element = e.target.value.toLowerCase();
+    if (element.length >= 3) {
+      console.log(element);
+      document.getElementById("recipes").innerHTML = "";
+      recipes.filter((recipe) => {
+        if (recipe.appliance.toLowerCase().match(element)) {
+          //  displayRecipe(recipe);
+          console.log(recipe.appliance);
         }
       });
     }
   });
 };
 
-search();
+const searchIngredients = () => {
+  const searchUserIngredient = document.getElementById("searchIngredient");
+  searchUserIngredient.addEventListener("input", (e) => {
+    const element = e.target.value.toLowerCase();
+    if (element.length >= 3) {
+      console.log(element);
+      document.getElementById("recipes").innerHTML = "";
+      recipes.filter((recipe) => {
+        recipe.ingredients.forEach((el) => {
+          if (el.ingredient.toLowerCase().match(element)) {
+            // displayRecipe(recipe);
+            console.log(el.ingredient);
+          }
+        });
+      });
+    }
+  });
+};
+
+const searchUstensils = () => {
+  const searchUserUstensils = document.getElementById("searchUstensiles");
+  searchUserUstensils.addEventListener("input", (e) => {
+    const element = e.target.value.toLowerCase();
+    if (element.length >= 3) {
+      console.log(element);
+      document.getElementById("recipes").innerHTML = "";
+      recipes.filter((recipe) => {
+        recipe.ustensils.forEach((el) => {
+          if (el.toLowerCase().match(element)) {
+            // displayRecipe(recipe);
+            console.log(el);
+          }
+        });
+      });
+    }
+  });
+};
+
+principalSearch();
+searchIngredients();
+searchAppareil();
+searchUstensils();
